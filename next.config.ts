2b81bf5dedb42d753d68dabf372bcd102ff09a1c
Perrine Huon <webdesignbyperrine@gmail.com>
2025-12-10 21:57:29 +0100

    fix: Ajouter domaine Supabase Storage à next.config

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         hostname: 'images.unsplash.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'iuqljcfianyoffoaaqjb.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
     ],
   },
 };
